a8d4fb81890c454504671f75fad7995a0ac22f19
hendras1722 <muhsyahendraa1722@gmail.com>
2026-06-02 10:10:57 +0700

    refactor: rename TanStackRouterVite to tanstackRouter in vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => {
   return {
     plugins: [
       tailwindcss(),
-      TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
+      tanstackRouter({ target: 'react', autoCodeSplitting: true }),
       react()
     ],
     server: {
