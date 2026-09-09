7d16f02156b5422dd9a3482642b02d61baf6a964
hendras1722 <muhsyahendraa1722@gmail.com>
2026-09-09 10:00:04 +0700

    feat: add oxc-transform-react and enable react compiler in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
     tanstackRouter({ target: 'react', autoCodeSplitting: true }),
-    react(),
+    react({ compiler: true }),
     apiServerPlugin(),
   ],
 })
